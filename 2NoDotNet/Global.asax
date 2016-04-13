ef6ef6e55ba86cfff70d04595e897ecb14a37b08
Kailin Yang <Kailin Yang>
2016-04-13 23:04:16 +0800

    2NoInit

---
+++ 2NoDotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2NoDotNet.MvcApplication" Language="C#" %>
---
+++ 2NoDotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2NoDotNet.Startup))]
+namespace _2NoDotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
